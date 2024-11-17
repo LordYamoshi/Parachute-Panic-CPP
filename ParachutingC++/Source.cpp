4359f7d8a28126ced264d6ce85f5ca5b0582f4bd
Bashar <babbas107@gmail.com>
2024-11-17 20:10:18 +0100

    I didn't really change much

--- ParachutingC++/Source.cpp
+++ ParachutingC++/Source.cpp
@@ -2,9 +2,11 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
+    sf::RenderWindow window(sf::VideoMode(200, 200), "Parachute Panic C++");
+    sf::CircleShape shape(50.f);
     shape.setFillColor(sf::Color::Green);
+
+
 
     while (window.isOpen())
     {
@@ -13,11 +15,17 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                  window.close();
+
         }
+
+        shape.setOrigin(shape.getRadius(), shape.getRadius()); // Set origin to the center of the circle
+        shape.setPosition(window.getSize().x / 2.f, window.getSize().y / 2.f); // Center the circle in the window
 
         window.clear();
         window.draw(shape);
         window.display();
+
+
     }
 
     return 0;
