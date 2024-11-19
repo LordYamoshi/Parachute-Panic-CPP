1d98abd0f0fd8d3221695713173c4ccfec757fb5
Bashar <babbas107@gmail.com>
2024-11-19 17:33:27 +0100

    This is a working circle

--- ParachutingC++/Source.cpp
+++ ParachutingC++/Source.cpp
@@ -2,23 +2,12 @@
 
 int main()
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> parent of addcae7 (Not many changes, but it is pretty cool)
-=======
->>>>>>> parent of addcae7 (Not many changes, but it is pretty cool)
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
->>>>>>> parent of addcae7 (Not many changes, but it is pretty cool)
-=======
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
->>>>>>> parent of 4359f7d (I didn't really change much)
+    sf::RenderWindow window(sf::VideoMode(200, 200), "Parachute Panic C++");
+
+    sf::CircleShape shape(50.f);
     shape.setFillColor(sf::Color::Green);
+    shape.setOrigin(shape.getRadius(), shape.getRadius());
+    shape.setPosition(window.getSize().x / 2.f, window.getSize().y / 2.f);
 
     while (window.isOpen())
     {
@@ -26,29 +15,17 @@ int main()
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-                 window.close();
+                window.close();
+
+            if (event.type == sf::Event::Resized)
+            {
+                sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+                window.setView(sf::View(visibleArea));
+
+                shape.setPosition(event.size.width / 2.f, event.size.height / 2.f);
+            }
         }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        sf::Vector2u windowSize = window.getSize();
-
-        shape.setScale(
-            event.size.width / shape.getLocalBounds().width,
-            event.size.height / shape.getLocalBounds().height);
-        shape.setPosition(windowSize.x / 2, windowSize.y / 2);
-
-=======
->>>>>>> parent of addcae7 (Not many changes, but it is pretty cool)
-=======
->>>>>>> parent of 4359f7d (I didn't really change much)
-=======
->>>>>>> parent of addcae7 (Not many changes, but it is pretty cool)
-=======
->>>>>>> parent of addcae7 (Not many changes, but it is pretty cool)
         window.clear();
         window.draw(shape);
         window.display();
